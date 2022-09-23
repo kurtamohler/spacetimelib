8cd1a406eb15844884c39e323d1a5f209eab923d
Kurt Mohler <kurtamohler@gmail.com>
2022-09-23 13:53:43 -0500

    Delete submodules

--- spacetime/__init__.py
+++ spacetime/__init__.py
@@ -2,3 +2,8 @@ from .basic_ops import boost, proper_time
 from .frame import Frame2D, Clock
 from .worldline import Worldline
 from .error_checking import check
+
+del basic_ops
+del frame
+del worldline
+del error_checking
